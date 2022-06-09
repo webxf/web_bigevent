b92b13a71ccf394f82c40c861273e10872346d11
webxf <j17852533489@163.com>
2022-06-09 20:08:32 +0800

    完成

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -4,4 +4,9 @@
 $.ajaxPrefilter((options) => {
 // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
     options.url = `http://www.liulongbin.top:3007`+options.url
+    if(options.url.includes('/my/')){
+        options.headers = {
+            Authorization: localStorage.getItem('token'),
+        }
+    }
 })
